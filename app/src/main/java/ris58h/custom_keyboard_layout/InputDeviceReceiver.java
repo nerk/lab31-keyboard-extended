d1b1b131b4239c93602d30bd5366549004da6e40
Kern, Thomas <thomas@kernwelt.de>
2024-12-23 17:42:53 +0100

    initial commit

---
+++ app/src/main/java/ris58h/custom_keyboard_layout/InputDeviceReceiver.java
@@ -0,0 +1,12 @@
+package ris58h.custom_keyboard_layout;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class InputDeviceReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
---
+++ app/src/main/java/ris58h/custom_keyboard_layout/MainActivity.java
@@ -0,0 +1,6 @@
+package ris58h.custom_keyboard_layout;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity{
+}
